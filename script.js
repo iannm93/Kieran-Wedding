6e2e4d94d579736685ff37e159ca11fe3b2c5a12
Ian Harris <ianmharris93@gmail.com>
2022-08-04 16:04:33 -0700

    finished basic skeleton, made redirects for wedding registry, waiting to add desginer's fonts

---
+++ script.js
@@ -0,0 +1,6 @@
+let clickHere = document.getElementById("click-here")
+let windowDirect=()=>{
+    window.open("https://getbootstrap.com/docs/5.1/getting-started/introduction/")
+}
+
+clickHere.addEventListener("click", windowDirect)
