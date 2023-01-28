2246e32791b2a702ad32b450ec646fd437f339f3
ianm93 <45110918+iannm93@users.noreply.github.com>
2023-01-27 21:47:56 -0800

    deleted form added media queries

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ let clickHere = document.getElementById("click-here")
 let windowDirect=()=>{
     window.open("https://www.google.com")
 }
+let string = "abcdefghijklmnopqrstuvwxyz"
+console.log(string)
 
+let capitalizeEvens = (string) =>{
+    let newString = ""
+  for (let z =0; z <string.length; z++){
+    if(z%2 ===0){
+
+    newString += string[z].toUpperCase();
+      
+    }
+    else newString += string[z].toLowerCase();
+}
+console.log(newString)
+}
+capitalizeEvens(string)
 clickHere.addEventListener("click", windowDirect)
