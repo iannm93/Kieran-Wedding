76624d8ed08d4db69c7ba3093d70a8437e8a1b58
ianm93 <45110918+iannm93@users.noreply.github.com>
2023-08-12 00:25:06 -0700

    removeed modals, styled rest of page / removed the unneeded information

--- script.js
+++ script.js
@@ -5,36 +5,48 @@ let si = document.querySelector(".img-target .specific-image");
 let my = document.querySelector(".myImg");
 
 let modalShow = (obj, event, modalC, modal, cards) => {
-  // console.log(event.target)
-  console.log(event.target.textContent)
-  let target = event.target.src;
-  console.log(target)
-  for (let i = 0; i < obj.length; i++) {
-    if (event.target.textContent === "×") {
-      console.log("test")
-      modal[i].style.display = "none";
-      return;
-    }
-    // console.log(event.target.attributes[1].nodeValue);
-    if (target.includes(obj[i].link)) {
-      console.log(i);
-      console.log(cards[i]);
-      // console.log(event.target.attributes[1].nodeValue);
-      if (cards[i].style.width === "15.65rem") {
-        modalC[i].style.width = "60%";
-      }
-      console.log(i);
-      console.log(modalC[i]);
-      console.log(modalC[i]);
+  let image_src = event.target.src
+  console.log(image_src)
+  console.log(obj[0].link)
+  for (let i = 0; i < obj.length; i++){
+    
+    if (image_src === obj[i].link)
+    window.open(image_src)
 
-      modalC[i].src = event.target.src;
-      modal[i].style.display = "block";
-    }
-    // if(event.target.atrributes.includes("closed")){
-    //   console.log("test")
-    //   modal[i].style.display = "none";
-    // }
   }
+
+  // console.log(event.target)
+  // let target = event.target.src;
+  // for (let i = 0; i < obj.length; i++) {
+  //   if (event.target.textContent === "×") {
+  //     modalC[i].style.display = "none";
+  //     modal[i].style.display = "none";
+
+  //     console.log(modal[0].style.display)
+  //     console.log(modalC[i].style.display)
+
+  //     return;
+  //   }
+  //   console.log(event.target.attributes[1].nodeValue);
+  //   else if (target.includes(obj[i].link)) {
+  //     console.log(i);
+  //     console.log(cards[i]);
+  //     console.log(event.target.attributes[1].nodeValue);
+  //     if (cards[i].style.width === "15.65rem") {
+  //       modalC[i].style.width = "60%";
+  //     }
+  //     console.log(i);
+  //     console.log(modalC[i]);
+  //     console.log(modalC[i]);
+
+  //     modalC[i].src = event.target.src;
+  //     modal[i].style.display = "block";
+  //   }
+  //   if(event.target.atrributes.includes("closed")){
+  //     console.log("test")
+  //     modal[i].style.display = "none";
+  //   }
+  // }
 };
 let api_controller = async () =>{
   
@@ -88,43 +100,19 @@ let api_controller = async () =>{
       i === 50
       ) {
         let html = `         
-        <div id="card" class="card" style="width: 15.65rem;">
+        <div id="card" class="card" style="width: 26rem;">
         
         <img class="specific-image" src="${obj[i].link}" class="card-img-top" alt="...">
-        <!-- MODAL STARTS HERE -->
-        <div id="myModal" class="modal">
-        
-        <!-- The Close Button -->
-        <span onclick="console.log("test")" class="close">×</span>
-        
-        <!-- Modal Content (The Image) -->
-        <img style="max-width:500px" class="modal-content" src="${obj[i].link}">
-        
-        <!-- Modal Caption (Image Text) -->
-        <div id="caption"></div>
-        </div>
-        <!-- MODAL ENDS HERE -->
+
         </div>
         `;
         allHtml.push(html);
       } else {
         let html2 = `         
-        <div id="card" class="card bigger-card" style="width: 35rem;">
+        <div id="card" class="card bigger-card" style="width: 58.5rem;">
         
         <img class="specific-image" src="${obj[i].link}" class="card-img-top" alt="...">
-        <!-- MODAL STARTS HERE -->
-        <div id="myModal" class="modal">
-        
-        <!-- The Close Button -->
-        <span onclick="document.getElementById('myModal').style.display='none'" class="close">×</span>
-        
-        <!-- Modal Content (The Image) -->
-        <img style = "width: 100% !important;" class="modal-content" src="${obj[i].link}">
-        
-        <!-- Modal Caption (Image Text) -->
-        <div id="caption"></div>
-        </div>
-        <!-- MODAL ENDS HERE -->
+
         </div>
         `;
         //  let cards = document.querySelectorAll(".card")
@@ -138,13 +126,15 @@ let api_controller = async () =>{
         eventTar.innerHTML = allHtml;
         
       }
+      // for modal generation you would need to pass these query selectors into modalShow 
+      // ^^ **  IF U DECIDE TO GO BACKTO MODALS ** ^^
       let modalC = document.querySelectorAll(".modal-content");
       let modal = document.querySelectorAll(".modal");
       let cards = document.querySelectorAll(".card");
       console.log(obj)
       eventTar.addEventListener("click", (event)=>{
         let obj = response.data.images
-        modalShow(obj, event, modalC, modal, cards)
+        modalShow(obj, event)
       });
     };
     imgr_api();
